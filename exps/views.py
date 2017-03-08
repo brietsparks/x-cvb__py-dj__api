05fd6fda0db817364c4a2ec24d0b1b728135191e
bsapaka <bws46@cornell.edu>
2017-03-08 00:15:34 -0600

    add drf ext mixins

--- exps/views.py
+++ exps/views.py
@@ -1,18 +1,19 @@
 from rest_framework import viewsets
+from rest_framework_extensions.mixins import NestedViewSetMixin
 from exps.models import Exp, Project, Contribution
 from exps.serializers import ExpSerializer, ProjectSerializer, ContributionSerializer
 
 
-class ExpViewSet(viewsets.ModelViewSet):
+class ExpViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
     queryset = Exp.objects.all()
     serializer_class = ExpSerializer
 
 
-class ProjectViewSet(viewsets.ModelViewSet):
+class ProjectViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
 
 
-class ContributionViewSet(viewsets.ModelViewSet):
+class ContributionViewSet(NestedViewSetMixin, viewsets.ModelViewSet):
     queryset = Contribution.objects.all()
     serializer_class = ContributionSerializer
